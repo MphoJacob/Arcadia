a89cb6a10c860d837892dacc8516b1b3a9a16ba6
MphoJacob <Mphojacool@gmail.com>
2024-05-30 18:33:43 +0200

    Update fully_booked.php

--- fully_booked.php
+++ fully_booked.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "id22185372_arcadiacong", "Arcadia123%", "id22185372_arcadiacong");
+$conn = new mysqli("f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "d0d2pweoaui1aloc", "miqd2lotp3n7o7c6", "vij8oxb41a7lpjg6");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
